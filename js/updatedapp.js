fb5e1227aafc8fcdddd3deb9f3cbe1fdcce34789
Imran <imrantipu90@yahoo.com>
2022-09-04 21:32:19 +0200

    api tested without key

--- js/updatedapp.js
+++ js/updatedapp.js
@@ -1,4 +1,4 @@
-const API_KEY = ``;
+const API_KEY = `d56b5956541282bcb22af37c44ff452d`;
 // data fetch from api
 const loadTemoperature = city =>{
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
@@ -9,11 +9,19 @@ const loadTemoperature = city =>{
 }
 // displayed temperature dynamacally
 const displayTemperature = temp =>{
-    const temperature = document.getElementById('temperature');
-    // console.log(temp.main.temp);  getting the temperature from api data
-    temperature.innerText = temp.main.temp;
-    // console.log(temp.weather[0].main);  getting the weather condition from api data
+    // const temperature = document.getElementById('temperature');
+    // // console.log(temp.main.temp);  getting the temperature from api data
+    // temperature.innerText = temp.main.temp;
+    // // console.log(temp.weather[0].main);  getting the weather condition from api data
+    setInnerTextById('temperature',temp.main.temp);
+    setInnerTextById('condition',temp.weather[0].main);
 }
+// getting id and setting innertext in one function
+const setInnerTextById = (id, text) =>{
+   const temperature = document.getElementById(id);
+    temperature.innerText = text;   
+}
+
 // talking search button input and updating only temperature
 document.getElementById('btn-search').addEventListener('click',function(){
     const searchField = document.getElementById('search-field');
