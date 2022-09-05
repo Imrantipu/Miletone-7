2e8555d9bd5365ede34dd0850533f0d54a8f23df
Imran <imrantipu90@yahoo.com>
2022-09-05 09:10:54 +0200

    used arrow function and delay time

--- js/async.js
+++ js/async.js
@@ -4,6 +4,8 @@ function doSAomething(){
 
 console.log('first');
 console.log('second');
-doSAomething();
+// doSAomething();
+setTimeout(doSAomething); /// this line will exicute asynsronously
+setTimeout(()=>{console.log('third')},5000); // used arrow function and delay time
 console.log('fourth');
 console.log('fifth');
