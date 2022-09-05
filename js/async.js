d3fafa3e1cb3fe2bcb93049a838929c3d9d4fec3
Imran <imrantipu90@yahoo.com>
2022-09-05 09:28:55 +0200

    fetch is algo asyncronus

--- js/async.js
+++ js/async.js
@@ -5,7 +5,20 @@ function doSAomething(){
 console.log('first');
 console.log('second');
 // doSAomething();
-setTimeout(doSAomething); /// this line will exicute asynsronously
-setTimeout(()=>{console.log('third')},5000); // used arrow function and delay time
+// setTimeout(doSAomething); /// this line will exicute asynsronously
+// setTimeout(()=>{console.log('third')},5000); // used arrow function and delay time
+
+// fetch data 
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => console.log(json))
+
+//   use consol log in fetch operation
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => {console.log('response recevied');
+return response.json();
+})
+  .then(json => console.log(json))
+
 console.log('fourth');
 console.log('fifth');
