daf3e93f1a2752789e385fb7f0eafa706ff716d5
Imran <imrantipu90@yahoo.com>
2022-09-11 09:25:55 +0200

    Block scope global scope simple understanding of Hoisting

---
+++ js/scope.js
@@ -0,0 +1,17 @@
+// function scope and block scope(if ,else)
+
+function add(a, b){
+    const total = a+b;
+    if(b>5){
+        const sum = 25 + a +b;
+    }
+    else{
+        const sum = 5 + a + b;
+        var current = sum;
+    }
+    console.log(current);
+    return total;
+}
+// console.log(a,b);
+// console.log(total);
+add(5, 3);
